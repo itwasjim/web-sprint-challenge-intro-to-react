5e914202dd9a59528c95cf66ed1747508aec2ef6
Tommy Duong <tduong0715@gmail.com>
2023-10-03 23:36:55 -0700

    Sprint Challenge finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,34 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState ([])
+
+  useEffect(() => {
+    axios
+    .get(urlPeople)
+    .then(res => {
+      setPeople(res.data)
+    })
+    .catch()
+    axios.get(urlPlanets)
+    .then(res => {
+      setPlanets(res.data)
+    })
+  }, [])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+
+    {people.map((people) => {
+      return (
+        <Character planets={planets} key={people.id} people={people}/>
+      )
+    })}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { 
+  // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+
+  const[render, setRender] = useState(false);
+
+  function togglePlanet(homeworld) {
+    return props.planets.map((planet) =>  {
+      if (planet.id === homeworld) {
+        return planet.name;
+      }
+    })
+  }
+
+  const toggle = () => {
+    setRender(!render);
+  };
+
   return (
-    <div>
+    <div className="character-card">
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 onClick={() => toggle()} className="character-name">
+        {props.people.name}
+      </h3>
+      {render ? (
+        <p>
+          Planet: {" "}
+          {
+            <span className="character-planet">
+              {" "}
+              {togglePlanet(props.people.homeworld)}
+            </span>
+          }
+        </p>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
 export default Character
